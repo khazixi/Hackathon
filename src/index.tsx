4ac60a029dccc56d53026f1b4baf6586ec290c0a
Khazixi <khazixfangirl@gmail.com>
2023-11-11 13:32:45 -0500

    Added HTMX to tag

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ const app = new Hono()
 app.get('/', (c) => c.html(
   <html>
     <head>
+      <script src="https://unpkg.com/htmx.org@1.9.8" integrity="sha384-rgjA7mptc2ETQqXoYC3/zJvkU7K/aP44Y+z7xQuJiVnB/422P/Ak+F/AqFR7E4Wr" crossorigin="anonymous"></script>
       <title> HI </title>
     </head>
     <body>
