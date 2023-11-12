68fbacf9a185c539c8292612611ce0184c28b19e
Takuto Ban <takutoban22@gmail.com>
2023-11-11 19:38:46 -0500

    Implement image rendering

--- src/routes/content.tsx
+++ src/routes/content.tsx
@@ -5,11 +5,14 @@ import { eq } from 'drizzle-orm';
 
 export const content = new Hono();
 
+const toBase64 = (buf: Buffer) => btoa(buf.reduce((data: string, byte: number) => data + String.fromCharCode(byte), ""));
+
 const postContent = (p: Post) => (
     <div>
         <h1>{p.title}</h1>
         <p>{`By ${p.author}: ${p.city}, ${p.state}`}</p>
         <p>{p.description}</p>
+        {p.image && <img src={"data:image/png;base64," + toBase64(p.image)} alt={`${p.title} Image`}/> }
     </div>
 );
 
